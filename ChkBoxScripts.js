de0e28e1730999300019f2b941f90361497e384a
William Alexander <wralex@gmail.com>
2023-01-30 18:19:59 -0500

    Added Javascript function and updated lab18

--- ChkBoxScripts.js
+++ ChkBoxScripts.js
@@ -1,17 +1,41 @@
-$(document).ready( function(){
-    // read the current/previous setting
-     $("input.box[type=checkbox]").each(function() {
-         var name = $(this).attr('name');
-         if ($.cookie(name) && $.cookie(name) == "true") {
-             $(this).prop('checked', $.cookie(name));
-         }
-     });
-    // event management
-     $("input.box[type=checkbox]").change(function() {
-         var name = $(this).attr("name");
-         $.cookie(name, $(this).prop('checked'), {
-             path: '/',
-             expires: 365*30
-         });
-     });
- });
+$(document).ready(
+    function () {
+        
+        $("input.box[type=checkbox]").each(
+            function () {
+                let id = $(this).attr('id');
+                if ($.cookie(id) && $.cookie(id) == "true") {
+                    $(this).prop('checked', $.cookie(id));
+                }
+            }
+        );
+        
+        $("i.fa-clipboard").on(
+            "click",
+            function () {
+                let text = '';
+                let clipelem = $(this).attr('clip-elem');
+                if (clipelem) {
+                    text = $('#' + clipelem)[0].innerText;
+                } else {
+                    text = $(this).attr('clip-text');
+                }
+                navigator.clipboard.writeText(text)
+                .then(() => {})
+                .catch(err => {
+                    alert('Error in copying text: ', err);
+                });
+            }
+        );
+    }
+);
+
+$("input.box[type=checkbox]").change(
+    function () {
+        let id = $(this).attr("id");
+        $.cookie(id, $(this).prop('checked'), {
+            path: '/',
+            expires: 365 * 30
+        });
+    }
+);
